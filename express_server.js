236d0b821265ce8b1ca8ebf268697e26dbc2af5e
Andrew McNeill <andrew@andrewmc.ca>
2022-01-25 17:02:52 -0400

    added cookie-parser as a dependency

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = 8080; // default port 8080
