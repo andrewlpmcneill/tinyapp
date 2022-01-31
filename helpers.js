4c0130ccf5088f08c5b8805c9a694850cc160d30
Andrew McNeill <andrew@andrewmc.ca>
2022-01-30 21:42:03 -0400

    Adds comments for each helper function

--- helpers.js
+++ helpers.js
@@ -1,8 +1,10 @@
+// Produces a random alphanumeric (base 36) string of length 6
 const generateRandomString = () => {
-  // Produces a random alphanumeric (base 36) string of length 6
   return Math.random().toString(36).slice(2, 8);
 };
 
+// Returns user object by email in a given database object
+// Returns false if no user is found
 const getUserByEmail = (email, database) => {
   for (let user in database) {
     if (database[user]['email'] === email) {
@@ -12,6 +14,7 @@ const getUserByEmail = (email, database) => {
   return false;
 };
 
+// Returns a list of URLs created by a specified user
 const urlsForUser = (id, database) => {
   const userURLs = {};
   for (const url in database) {
@@ -22,6 +25,7 @@ const urlsForUser = (id, database) => {
   return userURLs;
 };
 
+// Creates a timestamp, formatted to month/day/year, time
 const timeStamp = () => {
   const milliseconds = Date.now(); // 1575909015000
   const dateObject = new Date(milliseconds);
